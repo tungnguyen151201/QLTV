9109f2a5754cde052278c3a0f17509f0a8734ab4
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2022-06-02 14:11:49 +0700

    Mô hình 3 lớp

---
+++ QLTV/DTO/DocGiaDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.DTO
+{
+    public class DocGiaDTO : NguoiDungDTO
+    {
+        public string MaDocGia { get; set; }
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public DateTime NgayLapThe { get; set; }
+        public int LoaiDocGia { get; set; }
+        public DocGiaDTO(string madocgia, string hoten, string diachi, DateTime ngaysinh, DateTime ngaylapthe, int loaidocgia)
+        {
+            MaDocGia = madocgia;
+            HoTen = hoten;
+            DiaChi = diachi;
+            NgaySinh = ngaysinh;
+            NgayLapThe = ngaylapthe;
+            LoaiDocGia = loaidocgia;
+        }
+    }
+}
---
+++ QLTV/DTO/NguoiDungDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.DTO
+{
+    public class NguoiDungDTO
+    {
+        public string MaNguoiDung { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public DateTime NgayTao { get; set; }
+        public int LoaiNguoiDung { get; set; }
+    }
+}
